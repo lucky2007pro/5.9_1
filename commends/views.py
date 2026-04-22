3aa0e27e793b5d7644effc5c55ebd3204de66869
lucky2007pro <usa20070302@gmail.com>
2026-04-22 23:39:57 +0500

    yangilanish

--- commends/views.py
+++ commends/views.py
@@ -1,18 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
-from .serializers import CommendSerializer
+from django.shortcuts import get_object_or_404
 from .models import Commend
-# Create your views here.
+from .serializers import CommendSerializer
 
-class CommendView(APIView):
+class CommendListCreateView(APIView):
     def get(self, request, format=None):
         commends = Commend.objects.all()
         serializer = CommendSerializer(commends, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-class CommendCreateView(APIView):
     def post(self, request, format=None):
         serializer = CommendSerializer(data=request.data)
         if serializer.is_valid():
@@ -20,8 +18,25 @@ class CommendCreateView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class CommendDetailView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Commend, pk=pk)
+
+    def get(self, request, pk, format=None):
+        commend = self.get_object(pk)
+        serializer = CommendSerializer(commend)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     def put(self, request, pk, format=None):
-        commend = Commend.objects.get(pk=pk)
+        commend = self.get_object(pk)
+        serializer = CommendSerializer(commend, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk, format=None):
+        commend = self.get_object(pk)
         serializer = CommendSerializer(commend, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
@@ -29,14 +44,6 @@ class CommendCreateView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
-        commend = Commend.objects.get(pk=pk)
+        commend = self.get_object(pk)
         commend.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def patch(self, request, pk, format=None):
-        commend = Commend.objects.get(pk=pk)
-        serializer = CommendSerializer(commend, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- users/views.py
+++ users/views.py
@@ -1,23 +1,52 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
-# Create your views here.
+from django.shortcuts import get_object_or_404
+from .models import User
+from .serializers import UserSerializer
 
-class UserView(APIView):
+
+class UserListCreateView(APIView):
+
     def get(self, request, format=None):
-        return Response({'message': 'Hello, World!'}, status=status.HTTP_200_OK)
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, format=None):
-        return Response({'message': 'User created successfully!'}, status=status.HTTP_201_CREATED)
-
-    def put(self, request, format=None):
-        return Response({'message': 'User updated successfully!'}, status=status.HTTP_200_OK)
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserDetailView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(User, pk=pk)
+
     def get(self, request, pk, format=None):
-        return Response({'message': f'User {pk} retrieved successfully!'}, status=status.HTTP_200_OK)
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
-        return Response({'message': f'User {pk} deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
