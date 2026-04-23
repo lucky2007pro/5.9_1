b73069cd6a6abc0c643577d75a4774fbd0d2878b
lucky2007pro <usa20070302@gmail.com>
2026-04-23 10:33:17 +0500

    crtvbyhuj

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        return super().create(validated_data)
